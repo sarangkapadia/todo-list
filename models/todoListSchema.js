cd25680d22914334240c67c06ae7c32aa19d7b34
Sarang Kapadia <sarangksk@gmail.com>
2024-03-07 18:22:57 -0800

    add todo schema/model

---
+++ models/todoListSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const todoListSchema = new mongoose.Schema({
+    task: { type: String, required: true },
+    status: { type: Boolean, required: true },
+    dueBy: { type: Date, required: true },
+    priority: { type: Number, required: true }
+}, { timestamps: true });
+
+const todoUsersSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    todos: { type: [todoListSchema], required: true }
+}, { timestamps: true });
+
+const todoList = new monogoose.model('TodoList', todoListSchema, 'TodoList');
+const todoUsers = new mongoose.model('TodoUsers', todoUsersSchema, 'TodoUsers');
+
+module.exports = { todoList, todoUsers };
--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.listen(process.env.PORT || 3000);
+// mongoose
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
 
+const dbconnect = async () => {
+    const databaseName = 'Todo';
+    const uri = `mongodb+srv://sarangksk:sarmongodb83@clustertodo.ms3dfbo.mongodb.net/${databaseName}?retryWrites=true&w=majority&appName=ClusterTodo`;
+    try {
+        await mongoose.connect(uri);
+        console.log('Success connecting to MongoDB', databaseName);
+        app.listen(port, () => console.log(`Listening on port ${port}`));
+    } catch (err) {
+        console.log('Error Conncecting to MongoDB', err);
+    }
+}
+
+dbconnect();
+
+// express Apis
+app.get(('/user/id:'), (req, res) => {
+
+});
+
+app.use((req, res) => {
+    res.status(404).send('404! Does not exist');
+})
